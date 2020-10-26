3b8aee0f9c414a5ab461c6a2aa95e537f1b9214a
Russell Sutter <russell.sutter@gmail.com>
2020-10-26 22:11:27 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -38,4 +38,4 @@ const mapStateToProps = (state) => {
   return { items: state.items }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ state: state.items }))(App);
